eb2d7d556dbaa251324e0975a9c75cf794c62601
Francis Masse <francis.masse05@gmail.com>
2016-10-01 22:01:34 -0400

    Adding a second test as example.

--- src/controller_mission/state/move.py
+++ src/controller_mission/state/move.py
@@ -15,7 +15,11 @@ class Move(smach.State):
         rospy.loginfo('Executing state MOVE')
         if userdata.direction == 1 and userdata.distance_in > 0:
             userdata.distance_out = userdata.distance_in - 1
-            rospy.loginfo('distance remaining = %f' % userdata.distance_in)
+            rospy.loginfo('surge distance remaining = %f' % userdata.distance_in)
+            return 'outcome1'
+        if userdata.direction == 2 and userdata.distance_in > 0:
+            userdata.distance_out = userdata.distance_in - 1
+            rospy.loginfo('sway distance remaining = %f' % userdata.distance_in)
             return 'outcome1'
         else:
             return 'outcome2'
--- test/state/test_move.py
+++ test/state/test_move.py
@@ -7,10 +7,19 @@ __author__ = 'Francis Masse'
 
 class TestMainExample(unittest.TestCase):
 
-    def test_move_forward(self):
+    def test_surge(self):
         move = Move()
         test_sm = smach.StateMachine(outcomes=[])
         test_sm.userdata.direction = 1
         test_sm.userdata.distance_in = 1
         result = move.execute(test_sm.userdata)
         self.assertEqual('outcome1', result)
+
+    def test_sway(self):
+        move = Move()
+        test_sm = smach.StateMachine(outcomes=[])
+        test_sm.userdata.direction = 2
+        test_sm.userdata.distance_in = 1
+        result = move.execute(test_sm.userdata)
+        self.assertEqual('outcome1', result)
+

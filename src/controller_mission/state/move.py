7a3be477833f64e9fce7c8bd2ab2af17d49b4ce4
Francis Masse <francis.masse05@gmail.com>
2016-10-04 17:10:34 -0400

    Modifying test_move.py. Modifying move.py to fit test

--- src/controller_mission/state/move.py
+++ src/controller_mission/state/move.py
@@ -7,20 +7,25 @@ _author_ = 'Francis Masse'
 class Move(smach.State):
     def __init__(self):
         smach.State.__init__(self,
-                             outcomes=['outcome1', 'outcome2'],
+                             outcomes=['succeeded', 'not_reach', 'aborted'],
                              input_keys=['direction', 'distance_in'],
                              output_keys=['distance_out'])
 
     def execute(self, userdata):
         rospy.loginfo('Executing state MOVE')
-        if userdata.direction == 'Surge' and userdata.distance_in > 0:
-            userdata.distance_out = userdata.distance_in - 1
-            rospy.loginfo('surge distance remaining = %f' % userdata.distance_in)
-            return 'outcome1'
-        if userdata.direction == 'Heave' and userdata.distance_in > 0:
-            userdata.distance_out = userdata.distance_in - 1
-            rospy.loginfo('sway distance remaining = %f' % userdata.distance_in)
-            return 'outcome1'
+        if userdata.direction == 'Bow':
+            if userdata.distance_in > 0:
+                userdata.distance_out = userdata.distance_in - 1
+                rospy.loginfo('Bow distance remaining = %f' % userdata.distance_in)
+                return 'not_reach'
+            else:
+                return 'succeeded'
+        if userdata.direction == 'Stern':
+            if userdata.distance_in > 0:
+                userdata.distance_out = userdata.distance_in - 1
+                rospy.loginfo('Stern distance remaining = %f' % userdata.distance_in)
+                return 'not_reach'
+            else:
+                return 'succeeded'
         else:
-            return 'outcome2'
-
+            return 'aborted'
--- test/state/test_move.py
+++ test/state/test_move.py
@@ -9,48 +9,90 @@ class TestMainExample(unittest.TestCase):
 
     def test_bow(self):
         move = Move()
-        test_sm = smach.StateMachine(outcomes=[])
+        test_sm = smach.StateMachine(outcomes=['succeeded', 'aborted'])
         test_sm.userdata.direction = 'Bow'
-        test_sm.userdata.distance_in = 1
-        result = move.execute(test_sm.userdata)
-        self.assertEqual('outcome1', result)
+        test_sm.userdata.distance = 3
+        with test_sm:
+            smach.StateMachine.add('MOVE', Move(),
+                                   transitions={'succeeded': 'succeeded',
+                                                'not_reach': 'MOVE'},
+                                   remapping={'direction': 'direction',
+                                              'distance_in': 'distance',
+                                              'distance_out': 'distance'})
+        outcome = test_sm.execute()
+        self.assertEqual('succeeded', outcome)
 
     def test_stern(self):
         move = Move()
-        test_sm = smach.StateMachine(outcomes=[])
+        test_sm = smach.StateMachine(outcomes=['succeeded', 'aborted'])
         test_sm.userdata.direction = 'Stern'
-        test_sm.userdata.distance_in = 1
-        result = move.execute(test_sm.userdata)
-        self.assertEqual('outcome1', result)
+        test_sm.userdata.distance = 3
+        with test_sm:
+            smach.StateMachine.add('MOVE', Move(),
+                                   transitions={'succeeded': 'succeeded',
+                                                'not_reach': 'MOVE'},
+                                   remapping={'direction': 'direction',
+                                              'distance_in': 'distance',
+                                              'distance_out': 'distance'})
+        outcome = test_sm.execute()
+        self.assertEqual('succeeded', outcome)
 
     def test_port(self):
         move = Move()
-        test_sm = smach.StateMachine(outcomes=[])
+        test_sm = smach.StateMachine(outcomes=['succeeded', 'aborted'])
         test_sm.userdata.direction = 'Port'
-        test_sm.userdata.distance_in = 1
-        result = move.execute(test_sm.userdata)
-        self.assertEqual('outcome1', result)
+        test_sm.userdata.distance = 3
+        with test_sm:
+            smach.StateMachine.add('MOVE', Move(),
+                                   transitions={'succeeded': 'succeeded',
+                                                'not_reach': 'MOVE'},
+                                   remapping={'direction': 'direction',
+                                              'distance_in': 'distance',
+                                              'distance_out': 'distance'})
+        outcome = test_sm.execute()
+        self.assertEqual('succeeded', outcome)
 
     def test_starboard(self):
         move = Move()
-        test_sm = smach.StateMachine(outcomes=[])
+        test_sm = smach.StateMachine(outcomes=['succeeded', 'aborted'])
         test_sm.userdata.direction = 'Starboard'
-        test_sm.userdata.distance_in = 1
-        result = move.execute(test_sm.userdata)
-        self.assertEqual('outcome1', result)
-
-    def test_down(self):
-        move = Move()
-        test_sm = smach.StateMachine(outcomes=[])
-        test_sm.userdata.direction = 'Down'
-        test_sm.userdata.distance_in = 1
-        result = move.execute(test_sm.userdata)
-        self.assertEqual('outcome1', result)
+        test_sm.userdata.distance = 3
+        with test_sm:
+            smach.StateMachine.add('MOVE', Move(),
+                                   transitions={'succeeded': 'succeeded',
+                                                'not_reach': 'MOVE'},
+                                   remapping={'direction': 'direction',
+                                              'distance_in': 'distance',
+                                              'distance_out': 'distance'})
+        outcome = test_sm.execute()
+        self.assertEqual('succeeded', outcome)
 
     def test_up(self):
         move = Move()
-        test_sm = smach.StateMachine(outcomes=[])
+        test_sm = smach.StateMachine(outcomes=['succeeded', 'aborted'])
         test_sm.userdata.direction = 'Up'
-        test_sm.userdata.distance_in = 1
-        result = move.execute(test_sm.userdata)
-        self.assertEqual('outcome1', result)
+        test_sm.userdata.distance = 3
+        with test_sm:
+            smach.StateMachine.add('MOVE', Move(),
+                                   transitions={'succeeded': 'succeeded',
+                                                'not_reach': 'MOVE'},
+                                   remapping={'direction': 'direction',
+                                              'distance_in': 'distance',
+                                              'distance_out': 'distance'})
+        outcome = test_sm.execute()
+        self.assertEqual('succeeded', outcome)
+
+    def test_down(self):
+        move = Move()
+        test_sm = smach.StateMachine(outcomes=['succeeded', 'aborted'])
+        test_sm.userdata.direction = 'Down'
+        test_sm.userdata.distance = 3
+        with test_sm:
+            smach.StateMachine.add('MOVE', Move(),
+                                   transitions={'succeeded': 'succeeded',
+                                                'not_reach': 'MOVE'},
+                                   remapping={'direction': 'direction',
+                                              'distance_in': 'distance',
+                                              'distance_out': 'distance'})
+        outcome = test_sm.execute()
+        self.assertEqual('succeeded', outcome)

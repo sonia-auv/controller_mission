ff121909cada6594fb1382138fce52df8d64f4cf
Francis Masse <francis.masse05@gmail.com>
2016-10-05 12:36:11 -0400

    Add executable to controller_mission

--- src/controller_mission/main.py
+++ src/controller_mission/main.py
@@ -1,31 +1,35 @@
+#!/usr/bin/env python
+
 import rospy
 import smach
 from state.move import Move
+from smach_ros import SimpleActionState
 
-_author_ = 'Francis Masse'
+__author__ = 'Francis Masse'
 
 
 def main():
     rospy.init_node('controller_mission')
 
     # Create a SMACH state machine
-    sm = smach.StateMachine(outcomes=['outcome3', 'outcome4'])
-    sm.userdata.sm_direction = 'Surge'
+    sm = smach.StateMachine(['succeeded', 'aborted', 'preempted'])
+    sm.userdata.sm_direction = 'Bow'
     sm.userdata.sm_distance = 5
 
     # Open the container
     with sm:
         # Add states to the container
-        smach.StateMachine.add('MOVE', Move(),
-                               transitions={'outcome1': 'MOVE',
-                                            'outcome2': 'outcome3'},
+        smach.StateMachine.add('MOVE',
+                               Move(),
+                               transitions={'succeeded': 'succeeded',
+                                            'not_reach': 'MOVE',
+                                            'aborted': 'aborted'},
                                remapping={'direction': 'sm_direction',
-                                          'distance_in': 'sm_distance',
-                                          'distance_out': 'sm_distance'})
+                                          'distance': 'sm_distance'})
 
     # Execute SMACH plan
     outcome = sm.execute()
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()

6aa7602f6c65045dce64505fc3c3dd873810933e
Francis Masse <francis.masse05@gmail.com>
2016-12-04 19:54:22 -0500

    Dynamic_reconfig working with state move

--- src/controller_mission/main.py
+++ src/controller_mission/main.py
@@ -4,43 +4,44 @@ import rospy
 import smach
 import smach_ros
 from state.move import Move
-from proc_control.msg import TargetReached
-from geometry_msgs.msg import Pose
+from dynamic_reconfigure.server import Server
+from controller_mission.cfg import SquareTestConfig
 
 
 __author__ = 'Francis Masse'
 
 
-def target_reach_cb(data):
-    if data:
-        main_sm.userdata.position_reached = 1
+def square_test_cb(config, level):
+    rospy.loginfo("""Reconfigure Request: {set_position_x}, {set_position_y}, {set_position_z}, \
+                  """.format(**config))
+    return config
 
 
 def main():
     rospy.init_node('controller_mission')
-    rospy.Subscriber('local_target_reach', TargetReached, target_reach_cb)
+    dynamic_reconf_server = Server(SquareTestConfig, square_test_cb)
 
     # Create a SMACH state machine
     main_sm = smach.StateMachine(['succeeded', 'aborted'])
 
-    main_sm.userdata.position_reached = 0
-
     # Open the container
     with main_sm:
+
         # Add states to the container
         smach.StateMachine.add('MOVE_FORWARD',
-                               Move(),
+                               Move(dynamic_reconf_server),
                                transitions={'succeeded': 'MOVE_BACKWARD',
-                                            'aborted': 'aborted'},
-                               remapping={'position_reached': 'position_reached'})
+                                            'position_not_reach': 'MOVE_FORWARD',
+                                            'aborted': 'aborted'})
         smach.StateMachine.add('MOVE_BACKWARD',
-                               Move(),
-                               transitions={'succeeded': 'succeeded',
-                                            'aborted': 'aborted'},
-                               remapping={'position_reached': 'position_reached'})
+                               Move(dynamic_reconf_server),
+                               transitions={'succeeded': 'MOVE_FORWARD',
+                                            'position_not_reach': 'MOVE_BACKWARD',
+                                            'aborted': 'aborted'})
 
     sis = smach_ros.IntrospectionServer('controller_mission_server', main_sm, '/controller_mission')
     sis.start()
+
     # Execute SMACH plan
     outcome = main_sm.execute()
 
--- src/controller_mission/state/move.py
+++ src/controller_mission/state/move.py
@@ -1,54 +1,51 @@
 import rospy
 import smach
-from nav_msgs.msg import Odometry
-from dynamic_reconfigure.server import Server
-from controller_mission.cfg import SquareTestConfig
 from proc_control.srv import SetPositionTarget
+from proc_control.msg import TargetReached
 
 _author_ = 'Francis Masse'
 
 
-def square_test_cb(config, level):
-    rospy.loginfo("""Reconfigure Request: {set_position_x}, {set_position_y}, {set_position_z}, \
-                  {set_position_roll}, {set_position_pitch}, {set_position_yaw}, """.format(**config))
-    return config
-
-
 class Move(smach.State):
-    def __init__(self):
+    def __init__(self, config):
         smach.State.__init__(self,
-                             outcomes=['succeeded', 'aborted'],
-                             input_keys=['position_reached'])
+                             outcomes=['succeeded', 'position_not_reach', 'aborted'])
+
+        target_reach_sub = rospy.Subscriber('/proc_control/target_reached', TargetReached, self.target_reach_cb)
+        self.target_reached = 0
+        self.server = config
+
+    def target_reach_cb(self, data):
+        self.target_reached = data.target_is_reached
 
     def execute(self, ud):
         rate = rospy.Rate(1)
-        dynamic_reconf_server = Server(SquareTestConfig, square_test_cb)
+
+        config = self.server.config
 
         rospy.wait_for_service('/proc_control/set_local_target')
         set_local_target = rospy.ServiceProxy('/proc_control/set_local_target', SetPositionTarget)
 
         rospy.loginfo('Executing state MOVE')
-        while not ud.position_reached:
-            try:
-                response = set_local_target(dynamic_reconf_server.config.set_position_x,
-                                            dynamic_reconf_server.config.set_position_y,
-                                            dynamic_reconf_server.config.set_position_z,
-                                            dynamic_reconf_server.config.set_position_roll,
-                                            dynamic_reconf_server.config.set_position_pitch,
-                                            dynamic_reconf_server.config.set_position_yaw)
-            except rospy.ServiceException as exc:
-                rospy.loginfo('Service did not process request: ' + str(exc))
 
-            rospy.loginfo('Set position x = %f' % dynamic_reconf_server.config.set_position_x)
-            rospy.loginfo('Set position y = %f' % dynamic_reconf_server.config.set_position_y)
-            rospy.loginfo('Set position z = %f' % dynamic_reconf_server.config.set_position_z)
-            rospy.loginfo('Set position roll = %f' % dynamic_reconf_server.config.set_position_roll)
-            rospy.loginfo('Set position pitch = %f' % dynamic_reconf_server.config.set_position_pitch)
-            rospy.loginfo('Set position yaw = %f' % dynamic_reconf_server.config.set_position_yaw)
+        try:
+            response = set_local_target(config.set_position_x,
+                                        config.set_position_y,
+                                        config.set_position_z,
+                                        0.0,
+                                        0.0,
+                                        0.0)
 
-            rate.sleep()
+        except rospy.ServiceException as exc:
+            rospy.loginfo('Service did not process request: ' + str(exc))
 
-        if ud.position_reached:
+        rospy.loginfo('Set position x = %f' % config.set_position_x)
+        rospy.loginfo('Set position y = %f' % config.set_position_y)
+        rospy.loginfo('Set position z = %f' % config.set_position_z)
+
+        rate.sleep()
+
+        if self.target_reached:
             return 'succeeded'
         else:
-            return 'aborted'
+            return 'position_not_reach'

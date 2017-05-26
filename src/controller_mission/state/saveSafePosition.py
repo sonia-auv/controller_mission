958fec897b151d7756981e0446dc88d178a09279
olavoie <olavoie9507@gmail.com>
2017-05-26 01:10:51 -0400

    adding saving save position state (not test with proc_mapping)

---
+++ src/controller_mission/state/saveSafePosition.py
@@ -0,0 +1,45 @@
+import rospy
+
+from ..mission_state import MissionState, Parameter
+from proc_mapping.msg import KeyValueIdPose
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Pose
+
+
+class SaveSafePosition(MissionState):
+
+    def __init__(self):
+        MissionState.__init__(self)
+        self.pub_safe_position = None
+        self.sub_act_pose = None
+        self.act_pose = Pose()
+        self.msg = KeyValueIdPose()
+        self.count = 0
+
+    def define_parameters(self):
+        self.parameters.append(Parameter('param_position_id', '1', 'Times Out'))
+
+    def get_outcomes(self):
+        return ['succeeded', 'aborted']
+
+    def pose_callback(self, data):
+        self.act_pose = data.pose.pose
+
+    def initialize(self):
+        self.pub_safe_position = rospy.Publisher('/proc_mapping/SavePose', KeyValueIdPose, queue_size=100)
+
+        self.sub_act_pose = rospy.Subscriber('/proc_navigation/odom', Odometry, self.pose_callback)
+
+    def run(self, ud):
+        self.msg.id = self.param_position_id
+        self.msg.pose = self.act_pose
+
+        self.pub_safe_position(self.msg)
+
+        self.count += 1
+
+        if self.count == 2:
+            return 'succeeded'
+
+    def end(self):
+        self.sub_act_pose.unregister()

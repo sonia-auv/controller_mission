262da778a4dc109df715dd7950e6b64f12bcd385
olavoie <olavoie9507@gmail.com>
2017-07-17 17:19:08 -0400

    adding DoActionSrv to drop droppers

--- src/controller_mission/state/dropperState.py
+++ src/controller_mission/state/dropperState.py
@@ -1,6 +1,7 @@
 import rospy
 
 from ..mission_state import MissionState, Parameter
+from provider_actuators.srv import DoActionSrv, DoActionSrvRequest
 
 
 class Droppers(MissionState):
@@ -8,6 +9,10 @@ class Droppers(MissionState):
     def __init__(self):
         MissionState.__init__(self)
         self.start_time = None
+        self.do_action = None
+
+        self.action = DoActionSrvRequest()
+        self.action_dic = {'1': self.action.SIDE_PORT, '2': self.action.SIDE_STARBOARD}
 
     def define_parameters(self):
         self.parameters.append(Parameter('param_id', 1, 'Times Out'))
@@ -16,10 +21,17 @@ class Droppers(MissionState):
         return ['succeeded', 'aborted', 'preempted']
 
     def initialize(self):
-        pass
+        rospy.wait_for_service('/provider_actuators/do_action_srv')
+        self.do_action = rospy.ServiceProxy('/provider_actuators/do_action_srv', DoActionSrv)
+        try:
+
+            self.do_action(self.action.ELEMENT_DROPPER, self.action_dic[str(int(self.param_id))], self.action.ACTION_DROPPER_LAUNCH)
+
+        except rospy.ServiceException as exc:
+            rospy.loginfo('Service did not process request: ' + str(exc))
 
     def run(self, ud):
-        rospy.loginfo('Dropper : %f is launch' % self.param_id)
+        rospy.loginfo('Dropper : %i is launch' % int(self.param_id))
         return 'succeeded'
 
     def end(self):

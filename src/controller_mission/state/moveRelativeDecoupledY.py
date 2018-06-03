4da67c1df383924ac51acf93b818b967f3a04339
coumarc9 <coumarc9@outlook.com>
2018-06-03 10:46:46 -0400

    Adding relative decoupled on y and corrections

--- src/controller_mission/state/moveRelativeDecoupledY.py
+++ src/controller_mission/state/moveRelativeDecoupledY.py
@@ -1,18 +1,17 @@
 import rospy
+
 from ..mission_state import MissionState, Parameter
 from proc_control.msg import TargetReached
 from proc_control.srv import SetDecoupledTarget
 
 
-class MoveDecoupledXY(MissionState):
+class MoveDecoupledRelativeY(MissionState):
 
     def __init__(self):
         MissionState.__init__(self)
-
         self.target_reached = False
 
     def define_parameters(self):
-        self.parameters.append(Parameter('param_distance_x', 1.0, 'Distance to travel'))
         self.parameters.append(Parameter('param_distance_y', 1.0, 'Distance to travel'))
 
     def get_outcomes(self):
@@ -22,23 +21,18 @@ class MoveDecoupledXY(MissionState):
         self.target_reached = data.target_is_reached
 
     def initialize(self):
-        rospy.wait_for_service('/proc_control/set_global_decoupled_target')
-        set_global_target = rospy.ServiceProxy('/proc_control/set_global_decoupled_target', SetDecoupledTarget)
-
-        self.target_reached = False
+        rospy.wait_for_service('/proc_control/set_local_decoupled_target')
+        set_local_target = rospy.ServiceProxy('/proc_control/set_local_decoupled_target', SetDecoupledTarget)
 
         try:
-            response = set_global_target(self.param_distance_x,
-                                         self.param_distance_y,
-                                         0, 0, 0, 0,
-                                         False, False, True, True, True, True)
-            self.target_reached = False
+            set_local_target(0, self.param_distance_y,
+                                   0, 0, 0, 0,
+                                  True, False, True, True, True, True)
+
         except rospy.ServiceException as exc:
             rospy.loginfo('Service did not process request: ' + str(exc))
 
-        rospy.loginfo('Set decoupled position x = %f' % self.param_distance_x)
-        rospy.loginfo('Set decoupled position y = %f' % self.param_distance_y)
-
+        rospy.loginfo('Set relative decoupled position y = %f' % self.param_distance_y)
         self.target_reach_sub = rospy.Subscriber('/proc_control/target_reached', TargetReached, self.target_reach_cb)
 
     def run(self, ud):

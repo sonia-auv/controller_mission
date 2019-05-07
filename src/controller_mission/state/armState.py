3a3b7018302b375b078dad3d03db9b7d78cce709
Lucas Mongrain <xeaprog@gmail.com>
2019-05-07 12:33:00 -0400

    add arm state

---
+++ src/controller_mission/state/armState.py
@@ -0,0 +1,38 @@
+import rospy
+
+from ..mission_state import MissionState, Parameter
+from proc_actuators.srv import cmActionSrv, cmActionSrvRequest
+
+
+class Arm(MissionState):
+
+    def __init_(self):
+        MissionState.__init__(self)
+        self.start_time = None
+        self.do_action = None
+
+        self.action = cmActionSrvRequest()
+        self.action_dic = {'0': self.action.CLOSE_ARM, '1': self.action.OPEN_ARM}
+
+    def define_parameters(self):
+        self.parameters.append(Parameter('param_id', 1, 'Times Out'))
+
+    def get_outcomes(self):
+        return ['succeeded', 'aborted', 'preempted']
+
+    def initialize(self):
+        rospy.wait_for_service('/proc_actuators/cm_action_srv')
+        self.do_action = rospy.ServiceProxy('/proc_actuators/cm_action_srv', cmActionSrv)
+        try:
+
+            self.do_action(self.action.ELEMENT_ARM, self.action_dic[str(int(self.param_id))], self.action.ACTION_ARM_EXEC)
+
+        except rospy.ServiceException as exc:
+            rospy.loginfo('Service did not process request: ' + str(exc))
+
+    def run(self, ud):
+        rospy.loginfo('arm : %i is executed' % int(self.param_id))
+        return 'succeeded'
+
+    def end(self):
+        pass

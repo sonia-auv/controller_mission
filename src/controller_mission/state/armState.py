2f6c42721348254ae7d2db5c6702f109d4148ed8
Lucas Mongrain <xeaprog@gmail.com>
2019-05-10 15:31:21 -0400

    bug fix arm

--- src/controller_mission/state/armState.py
+++ src/controller_mission/state/armState.py
@@ -6,13 +6,13 @@ from proc_actuators.srv import cmActionSrv, cmActionSrvRequest
 
 class Arm(MissionState):
 
-    def __init_(self):
+    def __init__(self):
         MissionState.__init__(self)
         self.start_time = None
         self.do_action = None
-
         self.action = cmActionSrvRequest()
-        self.action_dic = {'0': self.action.CLOSE_ARM, '1': self.action.OPEN_ARM}
+        self.action_dic = {'0': self.action.ARM_CLOSE,
+                           '1': self.action.ARM_OPEN}
 
     def define_parameters(self):
         self.parameters.append(Parameter('param_id', 1, 'Times Out'))
@@ -22,13 +22,17 @@ class Arm(MissionState):
 
     def initialize(self):
         rospy.wait_for_service('/proc_actuators/cm_action_srv')
-        self.do_action = rospy.ServiceProxy('/proc_actuators/cm_action_srv', cmActionSrv)
+        self.do_action = \
+            rospy.ServiceProxy('/proc_actuators/cm_action_srv',
+                               cmActionSrv)
         try:
+            self.do_action(self.action.ELEMENT_ARM,
+                           self.action_dic[str(int(self.param_id))],
+                           self.action.ACTION_ARM_EXEC)
+        except rospy.ServiceException, exc:
 
-            self.do_action(self.action.ELEMENT_ARM, self.action_dic[str(int(self.param_id))], self.action.ACTION_ARM_EXEC)
-
-        except rospy.ServiceException as exc:
-            rospy.loginfo('Service did not process request: ' + str(exc))
+            rospy.loginfo('Service did not process request: '
+                          + str(exc))
 
     def run(self, ud):
         rospy.loginfo('arm : %i is executed' % int(self.param_id))

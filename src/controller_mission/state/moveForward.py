16411e985cf664ab37837743ed3aee5e9874092b
kamoss <camille.sauvain.1@etsmtl.net>
2019-06-29 16:15:55 -0400

    new forward state

---
+++ src/controller_mission/state/moveForward.py
@@ -0,0 +1,42 @@
+import rospy
+
+from ..mission_state import MissionState, Parameter
+from proc_control.srv import SetPositionTarget
+
+
+class MoveSpeed(MissionState):
+
+    def __init__(self):
+        MissionState.__init__(self)
+        self.set_local_target = None
+
+        self.target_reached = False
+
+    def define_parameters(self):
+        self.parameters.append(Parameter('param_distance_x', 1.0, 'Distance to travel'))
+
+    def get_outcomes(self):
+        return ['succeeded', 'aborted', 'preempted']
+
+    def initialize(self):
+        rospy.wait_for_service('/proc_control/set_local_target')
+        self.set_local_target = rospy.ServiceProxy('/proc_control/set_local_target', SetPositionTarget)
+
+        try:
+            self.set_local_target(self.param_distance_x,
+                                  0.0,
+                                  0.0,
+                                  0.0,
+                                  0.0,
+                                  0.0)
+        except rospy.ServiceException as exc:
+            rospy.loginfo('Service did not process request: ' + str(exc))
+
+        rospy.loginfo('Set speed x = %f' % self.param_distance_x)
+
+    def run(self, ud):
+        if self.target_reached:
+            return 'succeeded'
+
+    def end(self):
+	return

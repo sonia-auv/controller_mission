9afab90955569fc9a8f84d653cb1e83e1534f44a
Marc-Antoine Couture <coumarc9@outlook.com>
2019-07-23 17:52:24 -0400

    Adding ya with switch states

---
+++ src/controller_mission/state/moveYawWithSwitch.py
@@ -0,0 +1,56 @@
+import rospy
+
+from ..mission_state import MissionState, Parameter
+from proc_control.msg import TargetReached
+from proc_control.srv import SetPositionTarget, SetControlMode, SetControlModeRequest
+
+
+class MoveYawWithSwitch(MissionState):
+
+    def __init__(self):
+        MissionState.__init__(self)
+        self.target_reached = False
+
+        self.mode = SetControlModeRequest()
+        self.mode_dic = {'0': self.mode.PositionModePID, '1': self.mode.PositionModePPI, '2': self.mode.VelocityModeB}
+
+    def define_parameters(self):
+        self.parameters.append(Parameter('param_yaw', 1.0, 'Distance to travel'))
+
+    def get_outcomes(self):
+        return ['succeeded', 'aborted', 'preempted']
+
+    def target_reach_cb(self, data):
+        self.target_reached = data.target_is_reached
+
+    def initialize(self):
+
+        rospy.wait_for_service('/proc_control/set_control_mode')
+        self.set_mode = rospy.ServiceProxy('/proc_control/set_control_mode', SetControlMode)
+        try:
+
+            self.set_mode(self.mode_dic[str(int(2))])
+
+        except rospy.ServiceException as exc:
+            rospy.loginfo('Service did not process request: ' + str(exc))
+
+        rospy.wait_for_service('/proc_control/set_global_target')
+        set_local_target = rospy.ServiceProxy('/proc_control/set_global_target', SetPositionTarget)
+
+        self.target_reached = False
+        try:
+            set_local_target(0.0, 0.0, 0.0, 0.0, 0.0, self.param_yaw,
+                             True, True, True, True, True, False)
+
+        except rospy.ServiceException as exc:
+            rospy.loginfo('Service did not process request: ' + str(exc))
+
+        rospy.loginfo('Set relative decoupled position yaw = %f' % self.param_yaw)
+        self.target_reach_sub = rospy.Subscriber('/proc_control/target_reached', TargetReached, self.target_reach_cb)
+
+    def run(self, ud):
+        if self.target_reached > 0:
+            return 'succeeded'
+
+    def end(self):
+        self.target_reach_sub.unregister()

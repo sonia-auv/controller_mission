88b7902577edb66c2ee1aecd3d24357f6190d1a0
Alexandre Desgagné <alexandre1998@live.ca>
2019-11-30 23:07:54 -0500

    Fix meter

--- src/controller_mission/state/alignAlexFrank.py
+++ src/controller_mission/state/alignAlexFrank.py
@@ -147,7 +147,7 @@ class AlignAlexFrank(MissionState):
 
     def align_depth(self):
         self.switch_control_mode(0)
-        self.z_adjustment = -(self.averaging_vision_y_pixel + abs(self.y_bounding_box.center_y) / (self.param_image_height / 2) + abs(self.y_bounding_box.center_y)) * self.basic_z_adjustment
+        self.z_adjustment = (-(self.averaging_vision_y_pixel + abs(self.y_bounding_box.center_y) / (self.param_image_height / 2) + abs(self.y_bounding_box.center_y)) * self.basic_z_adjustment)/1000
         rospy.loginfo('Z adjustment: ' + str(self.z_adjustment))
         # Take the highest value between min and the calculated adjustment and keep the sign
         self.z_adjustment = self.z_adjustment if abs(self.z_adjustment) >= (self.z_adjustment/abs(self.z_adjustment)) * \
